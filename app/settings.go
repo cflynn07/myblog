e6c6127f09e4e5ab1f711ef7f96bd65cfdcd199d
Casey Flynn <cflynn.us@gmail.com>
2019-03-04 16:19:13 +0800

    Add conditional include GA and fix test command in CI

--- app/settings.go
+++ app/settings.go
@@ -2,19 +2,22 @@ package app
 
 import (
 	"html/template"
+	"os"
 )
 
 // Template variables for all pages
 type globalPageVars struct {
-	Title       string
-	Host        string
-	Keywords    string
-	Description template.HTML
+	Title           string
+	Host            string
+	Keywords        string
+	Description     template.HTML
+	GoogleAnalytics string
 }
 
 var gpv = globalPageVars{
-	Title:       "Casey Flynn",
-	Host:        "https://cflynn.us",
-	Keywords:    "Casey Flynn, blog, web development, programming, digital nomad",
-	Description: template.HTML("Casey Flynn's ditital nomad programming, web development blog."),
+	Title:           "Casey Flynn",
+	Host:            "https://cflynn.us",
+	Keywords:        "Casey Flynn, blog, web development, programming, digital nomad",
+	Description:     template.HTML("Casey Flynn's ditital nomad programming, web development blog."),
+	GoogleAnalytics: os.Getenv("GOOGLE_ANALYTICS"),
 }
