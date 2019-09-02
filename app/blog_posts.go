91b9ea34c4e448ddb4336cc2026d524d737e4005
Casey Flynn <cflynn.us@gmail.com>
2019-09-02 11:23:23 +0800

    Update blog post

--- app/blog_posts.go
+++ app/blog_posts.go
@@ -21,11 +21,16 @@ type blogPosts map[string]*postData
 // Blog uses this static map to display blog posts, the keys should match files
 // in the template folder (sans the extension)
 var bp = blogPosts{
+	"2019-08-30-automatically-initialized-and-version-controlled-database-in-kubernetes-and-helm-development-environments": &postData{
+		Title:       "Automatically initalized and version controlled MySQL database in kubernetes and helm development environment",
+		Description: template.HTML("A simple setup for an automatically initialized and version controlled MySQL database in kubernetes & helm development environments"),
+		Keywords:    []string{"kubernetes", "helm", "skaffold", "development environment", "development environment mysql"},
+		Date:        time.Date(2019, time.August, 30, 0, 0, 0, 0, time.UTC),
+	},
 	"2019-02-26-website-in-a-binary": &postData{
 		Title:       "Bundling static website assets in a single binary with gobuffalo/packr",
 		Description: template.HTML("How to use gobuffalo/packr to build a simple website in golang with static assets that's easy to test and can be bundled into a single binary for deployment."),
 		Keywords:    []string{"golang", "go", "packr", "gobuffalo", "static assets"},
-		Date:        time.Date(2019, time.February, 28, 0, 0, 0, 0, time.UTC),
 	},
 	"2019-03-04-helm-update-with-new-values": &postData{
 		Title:       "Deploying with helm from CI using --reuse-values and adding new values to your values.yaml file",
@@ -39,6 +44,7 @@ var bpKeys = make([]string, len(bp))
 
 func init() {
 	bpKeys = []string{
+		"2019-08-30-automatically-initialized-and-version-controlled-database-in-kubernetes-and-helm-development-environments",
 		"2019-03-04-helm-update-with-new-values",
 		"2019-02-26-website-in-a-binary",
 	}
