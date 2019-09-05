4c38b843913153e58c3c61c7ee0d135bbe94eac2
Casey Flynn <cflynn.us@gmail.com>
2019-09-05 18:44:03 +0800

    Testing

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ func main() {
 
 	router.PathPrefix("/").HandlerFunc(app.CatchAllHandler)
 
+	log.Println("test")
 	log.Println("Listening on port " + os.Getenv("PORT"))
 	http.ListenAndServe(":"+os.Getenv("PORT"), router)
 }
