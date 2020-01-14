d180946be2a4739adeff1688597bd772b9a725ba
Casey Flynn <cflynn.us@gmail.com>
2020-01-14 22:18:26 +0800

    test

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+overrides=(
+  "develop_image=$HOME"
+  "deployment_time=\\\"$(date)\\\""
+  "deployment_sha=bang"
+  "action_id=fly"
+)
+
+echo "0: ${overrides[*]}"
+overrides=$(for i in "${overrides[@]}"; do echo -n "$i,"; done)
+echo "1: $overrides"
+echo "2: ${#overrides}"
+echo "3: ${overrides:0:${#overrides}-1}"
