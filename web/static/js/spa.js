1ae7fdceeb577715bbf5a4e7e6d63646c030a056
Casey Flynn <cflynn.us@gmail.com>
2020-04-03 16:21:29 +0800

    Add a lazy little script to make website load content without page refresh

---
+++ web/static/js/spa.js
@@ -0,0 +1,36 @@
+/*
+ * Just a super lazy script snippet to make my blog more SPA-like
+*/
+
+(() => {
+  function fetchPath (path) {
+    fetch(window.location.origin + path)
+      .then((result) => result.text())
+      .then((result) => {
+        const newDocument = new DOMParser().parseFromString(result, 'text/html')
+        const innerHTML = newDocument.querySelector('#maincontent').innerHTML
+        document.querySelector('#maincontent').innerHTML = innerHTML
+        PR.prettyPrint()
+        window.scrollTo(0,0)
+        bindEventLinks()
+      })
+  }
+
+  function bindEventLinks () {
+    document.querySelectorAll('nav.navbar a, #maincontent a').forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault()
+        const path = e.currentTarget.attributes.href.value
+        window.history.pushState({}, '', path)
+        fetchPath(path)
+      })
+    })
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    bindEventLinks()
+    window.onpopstate = function (event) {
+      fetchPath(window.location.pathname)
+    }
+  })
+})()
