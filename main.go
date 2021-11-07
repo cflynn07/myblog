794b5d24b6714efcfd1ea5a5d6f4f7c39c1f035a
Casey Flynn <cflynn.us@gmail.com>
2021-11-07 10:48:45 +0700

    debug

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Healthcheck /healthz")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "ok")
 	})
